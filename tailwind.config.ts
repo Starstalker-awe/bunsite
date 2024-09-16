9232392ec231bd3efbb80ad4f3bedb002900370b
Starstalker <trevoryatesawesome@gmail.com>
2024-09-16 21:01:27 +0100

    Added Dockerfile, shadow-glow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ export default {
     ], // All the src files with tailwind classes
     theme: {
         extend: {
+            boxShadow: {
+                glow: "0 0 12px 3px #FF9B2B, 0 25px 50px -12px #A3A3A3"
+            },
         },
     },
     plugins: [
