a6b5849862519315adaf57caa57391e1cd93a16d
Starstalker <trevoryatesawesome@gmail.com>
2024-11-12 21:06:40 -0600

    Improved loading of js files

--- middleware/static.ts
+++ middleware/static.ts
@@ -27,7 +27,7 @@ class CacheManager {
 
     public getEtag(fname: string): string { return this.files[fname]! };
     public isFresh(etag: string | undefined): boolean { return Boolean(Object.keys(this.files).find(fname => this.files[fname] === etag))};
-    public findFile(start: string) { return Object.keys(this.files).find(real => real.startsWith(start))};
+    public findFile(start: string) { return Object.keys(this.files).find(real => real.split(".")[0] === start.split(".")[0])};
 };
 
 const Cache = await new CacheManager().build();
@@ -39,15 +39,15 @@ export default function staticFiles(app: Elysia) { return app
         if (!raw.includes("..")) {
             if (Cache.isFresh(headers["if-none-match"])) { return new Response(null, { status: 304 })};
             const compressed = raw.match(/(css|js)/)?.[0];
-            const cached = `${compressed ? "compressed/" : ""}${raw}${compressed ? ".br" : ""}`;
-            const real = raw.split(".").length < 2 ? Cache.findFile(raw) : cached;
-            if (!real) { return new Response(null, { status: 404 })};
-            const file = Bun.file(`${pdir}/${real}`);
+            const truedir = `${compressed ? "compressed/" : ""}${raw}${compressed ? ".br" : ""}`;
+            const etag = Cache.findFile(truedir);
+            if (!etag) { return new Response(null, { status: 404 })};
+            const file = Bun.file(`${pdir}/${etag}`);
             return await file.exists() ? new Response(file, { headers: {
                 "Content-Type": MIMETYPES[raw.split(".").at(-1)!],
                 ...compressed ? { "Content-Encoding": "br" } : {},
                 "Cache-Control": "no-cache",
-                "ETag": Cache.getEtag(real)
+                "ETag": Cache.getEtag(etag)
             }}) : new Response(null, { status: 404 });
         } else { return new Response(null, { status: 403 })};
     });
